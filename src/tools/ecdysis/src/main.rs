99e27cc176ba752bfd459a65fc6d0d45130a4530
Taylor Cramer <cramertj@google.com>
2025-04-23 17:09:13 -0700

    Add Ecdysis driver skeleton
    Co-authored-by: Tyler Mandry <tmandry@gmail.com>

--- src/tools/ecdysis/src/main.rs
+++ src/tools/ecdysis/src/main.rs
@@ -1,11 +1,103 @@
 #![feature(rustc_private)]
 
+extern crate rustc_ast;
 extern crate rustc_driver;
+extern crate rustc_hir;
+extern crate rustc_interface;
+extern crate rustc_middle;
+extern crate rustc_session;
+extern crate rustc_span;
 
-#[allow(unused)]
-use rustc_driver::run_compiler;
+use rustc_ast::Crate;
+use rustc_driver::{Compilation, catch_fatal_errors, run_compiler};
+use rustc_hir::intravisit::Visitor;
+use rustc_interface::interface::{Compiler, Config};
+use rustc_middle::hir;
+use rustc_middle::ty::TyCtxt;
+use rustc_middle::util::Providers;
+use rustc_session::Session;
+use rustc_session::parse::ParseSess;
+use rustc_span::fatal_error::FatalError;
+
+struct EcdysisCallbacks {
+    // FIXME probably some state :)
+}
+
+fn override_queries(_session: &Session, _providers: &mut Providers) {
+    // FIXME override as-needed? maybe we won't need this if we can feed everything?
+}
+
+#[expect(dead_code)]
+struct TyProviderCollector<'tcx> {
+    tcx: TyCtxt<'tcx>,
+    // parent_type_bodies: ,
+}
+
+impl<'tcx> Visitor<'tcx> for TyProviderCollector<'tcx> {
+    type NestedFilter = hir::nested_filter::All;
+    fn maybe_tcx(&mut self) -> Self::MaybeTyCtxt {
+        self.tcx
+    }
+    // FIXME: record all uses of `ProvidedTy` and their input type generics. We need to *try* to
+    // make a well-ordered list of what `ProvidedTy`s are dependent on what other `ProvidedTy`s so
+    // that we can build them in the right order, as well as recording the non-`ProvidedTy`s they
+    // reference so that we can produce their C++ bindings in the same order (breaking cycles w/
+    // forward declarations where possible).
+    //
+    // There's some similar logic today in cc_bindings_from_rs.
+}
+
+impl rustc_driver::Callbacks for EcdysisCallbacks {
+    fn config(&mut self, config: &mut Config) {
+        config.psess_created = Some(Box::new(|_psess: &mut ParseSess| {
+            // FIXME: mutate psess as-needed.
+            // psess.file_depinfo should be populated with the file paths of the C++ files.
+            //
+            // We could consider mutating the source map to contain the C++ files as well if we ever
+            // want to point to them from rustc spans.
+        }));
+        // FIXME: hash_untracked_state probably to make incremental work someday.
+        // FIXME: register_lints
+        // Q: why is `override_queries` a `fn` rather than `Box::FnOnce`? Why can't it have state?
+        config.override_queries = Some(override_queries);
+    }
+
+    fn after_crate_root_parsing(
+        &mut self,
+        _compiler: &Compiler,
+        _krate: &mut Crate,
+    ) -> Compilation {
+        // FIXME: collect any syntactic (pre-macro-expansion / name resolution!) misuse.
+        // probably nothing to do here for now.
+        Compilation::Continue
+    }
+
+    fn after_expansion<'tcx>(&mut self, _compiler: &Compiler, _tcx: TyCtxt<'tcx>) -> Compilation {
+        // FIXME(ecdysis): For each use of a `TyProvider` in a path, record its generic parameters.
+        // tcx.hir_walktoplevel_module(visitor);
+
+        // pluto incremental build for dynamic deps? https://www.pl.informatik.uni-mainz.de/files/2019/04/pluto-incremental-build.pdf
+        // tcx.at().create_def();
+        Compilation::Continue
+    }
+    fn after_analysis<'tcx>(&mut self, _compiler: &Compiler, _tcx: TyCtxt<'tcx>) -> Compilation {
+        Compilation::Continue
+    }
+}
 
 fn main() {
-    // FIXME(ecdysis)
-    println!("Hello, world!");
+    let rustc_args: Vec<String> = vec!["".into()];
+    let mut driver_callbacks = EcdysisCallbacks {};
+
+    // The Rust compiler unwinds with a special sentinel value to abort compilation on
+    // fatal errors. We use `catch_fatal_errors` to 1) catch such panics and
+    // translate them into a Result, and 2) resume and propagate other panics.
+    let catch_fatal_errors_result: Result<(), FatalError> =
+        catch_fatal_errors(|| run_compiler(&*rustc_args, &mut driver_callbacks));
+
+    match catch_fatal_errors_result {
+        Ok(()) => {}
+        // We can ignore the `Err` payloads because the error types have only one value.
+        _ => panic!("Errors reported by Rust compiler."),
+    };
 }
